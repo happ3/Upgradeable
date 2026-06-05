473f65537725e15d59f948ef2abe82b0d9e72728
lhp <809030013@qq.com>
2026-06-05 15:04:58 +0800

    update

--- script/UpgradeV2.s.sol
+++ script/UpgradeV2.s.sol
@@ -3,28 +3,28 @@ pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {console} from "forge-std/console.sol";
 import {TokenFaucetProxyAdmin} from "../src/transparent/TokenFaucetProxyAdmin.sol";
+// ✅ v5 正确导入路径
+import {ITransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {LogicV2} from "../src/transparent/LogicV2.sol";
 
 contract UpgradeV2 is Script {
-    // 填你上一轮部署出来的地址
-    address constant ADMIN_ADDR = 0xeC18aD6289F95084C355547560D4044F2610F245;
-    address constant PROXY_ADDR = 0x3b4adA4074846107743F92326E426ab6403D461D;
+    address constant ADMIN_ADDR = 0xa513E6E4b8f2a923D98304ec87F64353C4D5C853;
+    address constant PROXY_ADDR = 0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6;
 
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        //1.部署新版本逻辑合约V2
         LogicV2 implV2 = new LogicV2();
         console.log("New ImplV2:", address(implV2));
 
-        //2.调用ProxyAdmin.upgrade 升级代理指向V2
         TokenFaucetProxyAdmin admin = TokenFaucetProxyAdmin(ADMIN_ADDR);
-        admin.upgrade(
-            payable(PROXY_ADDR),
-            address(implV2)
-        );
+        // 地址强转接口，删掉payable
+        ITransparentUpgradeableProxy proxy = ITransparentUpgradeableProxy(PROXY_ADDR);
+        // 空data=仅升级，不执行初始化
+        admin.upgradeAndCall(proxy, address(implV2), "");
 
+        console.log("Upgrade complete!");
         vm.stopBroadcast();
     }
 }
